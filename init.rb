7fb0cdc21317c2d38c59d5e294517c8411d26f2c
Guillaume <guillaume.paquereau@gmail.com>
2016-08-13 12:44:30 +0200

    Change version number to 1.1.0

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :redmine_openid_provider do
   name 'Redmine Openid Provider plugin'
   author 'MIURA Toru'
   description 'This plugin enables Redmine to behave as an Open ID provider.'
-  version '1.0.0'
-  url 'https://github.com/buri17/redmine_openid_provider'
+  version '1.1.0'
+  url 'https://github.com/Papac85/redmine_openid_provider'
   author_url 'https://github.com/buri17'
 end
 
